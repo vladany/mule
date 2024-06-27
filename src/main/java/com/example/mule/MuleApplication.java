7a8a28569a66807ae48aea3cc8dc2b99767265bc
varzrelian <vlada.azrelian@qytera.de>
2024-06-27 18:58:57 +0200

    init

---
+++ src/main/java/com/example/mule/MuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuleApplication.class, args);
+	}
+
+}
